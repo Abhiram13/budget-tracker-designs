e232f2712ca1445cd16afb8d1381337d13767d32
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-11 11:10:02 +0530

    added preview in UIkit

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -1,9 +1,28 @@
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.green
+    }
+}
+
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        ViewController();
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+struct ViewControllerPreviews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
     }
 }
 
