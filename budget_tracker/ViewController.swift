5b7db1ea57de124769f596196077af6d81319d73
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-11 12:48:31 +0530

    horizantal arrange in stack view

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
 import SwiftUI
 
+struct Transaction {
+    let date: String;
+    let debit: Int;
+    let credit: Int;
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Data model: These strings will be the data for the table view cells
-    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+//    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    
+    let transactions: [Transaction] = [
+        Transaction(date: "2022-10-20", debit: 234, credit: 5644),
+        Transaction(date: "2022-12-12", debit: 67, credit: 678),
+        Transaction(date: "2022-11-16", debit: 89, credit: 45),
+    ]
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
@@ -25,27 +37,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.animals.count
+        return self.transactions.count
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)! as UITableViewCell;
         
-        var stack = UIStackView();
+        let stack = UIStackView();
+        cell.addSubview(stack);
+        cell.backgroundColor = UIColor.red;
+        
         stack.translatesAutoresizingMaskIntoConstraints = false;
         stack.axis = .horizontal;
+        stack.alignment = .fill;
+        stack.distribution = .fillEqually;
+        stack.spacing = 60
+        stack.backgroundColor = UIColor.green
+        stack.isLayoutMarginsRelativeArrangement = true;
+        stack.leadingAnchor.constraint(equalTo: cell.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true;
+        stack.trailingAnchor.constraint(equalTo: cell.safeAreaLayoutGuide.trailingAnchor, constant: 10).isActive = true;
+                
         
-        cell.addSubview(stack);
+        let dateLabel = UILabel();
+        dateLabel.text = self.transactions[indexPath.row].date;
         
-        var firstLabel = UILabel();
-        firstLabel.text = self.animals[indexPath.row];
+        let debitLabel = UILabel();
+        debitLabel.text = "\(self.transactions[indexPath.row].debit)";
         
-        var secondLabel = UILabel();
-        secondLabel.text = self.animals[indexPath.row];
+        let creditLabel = UILabel();
+        creditLabel.text = "\(self.transactions[indexPath.row].credit)";
         
-        stack.addArrangedSubview(firstLabel);
-        stack.addArrangedSubview(secondLabel);
+        stack.addArrangedSubview(dateLabel);
+        stack.addArrangedSubview(debitLabel);
+        stack.addArrangedSubview(creditLabel);
         
         return cell
     }
