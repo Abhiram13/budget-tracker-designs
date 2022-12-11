5a75042f462cc7eaed250d4fe0eb8f47eb961521
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-11 11:52:06 +0530

    added stack view as HStack

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -1,28 +1,58 @@
 import UIKit
 import SwiftUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var tableView: UITableView!
+    // Data model: These strings will be the data for the table view cells
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    var tableView: UITableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.green
-    }
-}
-
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> ViewController {
-        ViewController();
+        view.backgroundColor = UIColor.green;
+        
+        tableView.frame = CGRectMake(0, 0, UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        self.view.addSubview(tableView)
     }
     
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
     }
-}
-
-struct ViewControllerPreviews: PreviewProvider {
-    static var previews: some View {
-        ViewControllerRepresentable()
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)! as UITableViewCell;
+        
+        var stack = UIStackView();
+        stack.translatesAutoresizingMaskIntoConstraints = false;
+        stack.axis = .horizontal;
+        
+        cell.addSubview(stack);
+        
+        var firstLabel = UILabel();
+        firstLabel.text = self.animals[indexPath.row];
+        
+        var secondLabel = UILabel();
+        secondLabel.text = self.animals[indexPath.row];
+        
+        stack.addArrangedSubview(firstLabel);
+        stack.addArrangedSubview(secondLabel);
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
     }
 }
 
