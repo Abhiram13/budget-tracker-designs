cdc4478f4371582720caccfbb6d1c824a0bc5488
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-24 11:48:33 +0530

    added container for start using UIView

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -1,80 +1,28 @@
 import UIKit
 import SwiftUI
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let cellReuseIdentifier = "cell";
-    let tableView: UITableView = UITableView();
-    let transactionController: TransactionController = TransactionController();
-    var transactionsByMonth: [TransactionByDate] = [TransactionByDate(date: "", debit: 0, credit: 0, count: 0)];
+class ViewController: UIViewController {
+    let container: UIView = UIView();
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.addSubview(tableView)
-
-        tableView.frame = CGRectMake(0, 0, UIScreen.main.bounds.width, UIScreen.main.bounds.height)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
-        tableView.backgroundColor = UIColor.brown;
-        self.transactionsByMonth = self.transactionController.fetchByMonth(month: "12", year: "2022");
-//        self.transactionController.Insert(
-//            payload: Transaction(
-//                id: 2, amount: 879, description: "asasd", type: "debit", to_bank_id: "ashj", from_bank_id: "asj", category_id: "ajk", date: "2022-12-15", due: 0, row_id: "asdj"
-//            )
-//        )
-    }
+        super.viewDidLoad();
+        self.view.addSubview(container);
+        self.view.backgroundColor = .green;
         
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.transactionsByMonth.count
+        self.containerConstraints()
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70;
-    }
+    private func containerConstraints() -> Void {
+        container.translatesAutoresizingMaskIntoConstraints = false;
+        container.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 20).isActive = true;
+        container.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.5).isActive = true;
+        container.backgroundColor = .blue;
+        container.layer.cornerRadius = 10;
         
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)! as UITableViewCell;
-        
-        let stack = UIStackView();
-        cell.addSubview(stack);
-        cell.backgroundColor = UIColor.red;
-        
-        stack.translatesAutoresizingMaskIntoConstraints = false;
-        stack.axis = .horizontal;
-        stack.alignment = .center;
-        stack.distribution = .equalSpacing;
-        stack.backgroundColor = UIColor.green
-        stack.isLayoutMarginsRelativeArrangement = true;
-        stack.leadingAnchor.constraint(equalTo: cell.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true; // padding leading
-        stack.topAnchor.constraint(equalTo: cell.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true; // padding top
-        stack.widthAnchor.constraint(equalToConstant: 370).isActive = true; // width of stack view
-        stack.heightAnchor.constraint(equalToConstant: 40).isActive = true; // height of stack view
-
-        let dateLabel = UILabel();
-        dateLabel.text = self.transactionsByMonth[indexPath.row].date;
-        dateLabel.backgroundColor = UIColor.blue;
-        dateLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
-
-        let debitLabel = UILabel();
-        debitLabel.text = "\(self.transactionsByMonth[indexPath.row].debit)";
-        debitLabel.backgroundColor = UIColor.purple;
-        debitLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        let creditLabel = UILabel();
-        creditLabel.text = "\(self.transactionsByMonth[indexPath.row].credit)";
-        creditLabel.backgroundColor = UIColor.yellow;
-        creditLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        stack.addArrangedSubview(dateLabel);
-        stack.addArrangedSubview(debitLabel);
-        stack.addArrangedSubview(creditLabel);
-        
-        return cell;
-    }
-        
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tapped cell number \(self.transactionsByMonth[indexPath.row].date).")
-        navigationController?.pushViewController(DatewiseViewController(), animated: false)
+        NSLayoutConstraint.activate([
+            container.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            container.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+        ])
     }
 }
 
