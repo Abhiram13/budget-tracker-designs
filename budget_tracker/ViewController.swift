f7e7a41c4642a0bfdf6f27ef69b46fbae217c95b
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-24 12:48:38 +0530

    added ui views inside stack

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -4,6 +4,8 @@ import SwiftUI
 class ViewController: UIViewController {
     let container: UIView = UIView();
     let mainLabel: UILabel = UILabel();
+    let scheduleView: UIView = UIView();
+    let scheduleStack: UIStackView = UIStackView();
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -12,6 +14,7 @@ class ViewController: UIViewController {
         
         self.containerConstraints();
         self.mainLabelConstrainsts();
+        self.scheduleViewConstrainsts();
     }
     
     private func containerConstraints() -> Void {
@@ -21,6 +24,7 @@ class ViewController: UIViewController {
         container.backgroundColor = .blue;
         container.layer.cornerRadius = 10;
         container.addSubview(mainLabel);
+        container.addSubview(scheduleView);
         
         NSLayoutConstraint.activate([
             container.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
@@ -36,6 +40,45 @@ class ViewController: UIViewController {
             mainLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
             mainLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: 30)
         ])
+    }
+    
+    private func scheduleViewConstrainsts() -> Void {
+        scheduleView.translatesAutoresizingMaskIntoConstraints = false;
+        scheduleView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 50).isActive = true;
+        scheduleView.heightAnchor.constraint(equalToConstant: 100).isActive = true;
+        scheduleView.backgroundColor = .red;
+        scheduleView.layer.cornerRadius = 20;
+        scheduleView.addSubview(scheduleStack);
+        
+        NSLayoutConstraint.activate([
+            scheduleView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
+            scheduleView.topAnchor.constraint(equalTo: container.topAnchor, constant: 75),
+        ]);
+        
+        self.scheduleStackConstraints();
+    }
+    
+    private func scheduleStackConstraints() -> Void {
+        let dateField: UIView = UIView();
+        let infoField: UIView = UIView();
+        
+        scheduleStack.translatesAutoresizingMaskIntoConstraints = false;
+        scheduleStack.axis = .horizontal;
+        scheduleStack.isLayoutMarginsRelativeArrangement = true;
+        scheduleStack.widthAnchor.constraint(equalTo: scheduleView.widthAnchor, constant: 0).isActive = true;
+        scheduleStack.heightAnchor.constraint(equalTo: scheduleView.heightAnchor, constant: 0).isActive = true;
+        scheduleStack.addArrangedSubview(dateField);
+        scheduleStack.addArrangedSubview(infoField);
+        
+        dateField.widthAnchor.constraint(equalTo: scheduleStack.widthAnchor, constant: 50).isActive = true;
+        dateField.heightAnchor.constraint(equalToConstant: 100).isActive = true;
+        dateField.backgroundColor = .cyan;
+        
+        infoField.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 250).isActive = true;
+        infoField.heightAnchor.constraint(equalToConstant: 100).isActive = true;
+        infoField.backgroundColor = .magenta;
+        
+        
     }
 }
 
