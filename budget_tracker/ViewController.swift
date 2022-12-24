39e6bf7c97a92097b26c633ec5018006264b2aca
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-24 12:01:13 +0530

    added text label in container

--- budget_tracker/ViewController.swift
+++ budget_tracker/ViewController.swift
@@ -3,13 +3,15 @@ import SwiftUI
 
 class ViewController: UIViewController {
     let container: UIView = UIView();
+    let mainLabel: UILabel = UILabel();
     
     override func viewDidLoad() {
         super.viewDidLoad();
         self.view.addSubview(container);
         self.view.backgroundColor = .green;
         
-        self.containerConstraints()
+        self.containerConstraints();
+        self.mainLabelConstrainsts();
     }
     
     private func containerConstraints() -> Void {
@@ -18,10 +20,21 @@ class ViewController: UIViewController {
         container.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.5).isActive = true;
         container.backgroundColor = .blue;
         container.layer.cornerRadius = 10;
+        container.addSubview(mainLabel);
         
         NSLayoutConstraint.activate([
             container.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             container.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+        ])
+    }
+    
+    private func mainLabelConstrainsts() -> Void {
+        mainLabel.text = "Start in 5 minutes";
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false;
+        
+        NSLayoutConstraint.activate([
+            mainLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
+            mainLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: 30)
         ])
     }
 }
