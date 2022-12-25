fb39a438acf7e718bd278dfdb7e5fa6ec5c4efdc
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2022-12-25 15:39:33 +0530

    added uilabels in stack views

--- budget_tracker/Helpers/Colors.swift
+++ budget_tracker/Helpers/Colors.swift
@@ -9,4 +9,8 @@ extension UIColor {
     static var ThemeBlue: UIColor {
         return UIColor(red: 52/255, green: 77/255, blue: 228/255, alpha: 1);
     }
+    
+    static var ThemeGreen: UIColor {
+        return UIColor(red: 82/255, green: 182/255, blue: 110/255, alpha: 1);
+    }
 }
