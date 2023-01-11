62873811e7b8f0a1a2bcb352caa696ab079dfead
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2023-01-11 16:28:03 +0530

    updated tab bar

--- budget_tracker/TabBarController.swift
+++ budget_tracker/TabBarController.swift
@@ -11,6 +11,12 @@ class TabBarController: UITabBarController {
         homeViewController.title = "Home";
         calenderViewController.title = "Calender";
         
+        self.tabBar.backgroundColor = .white;
+        self.tabBar.layer.shadowColor = UIColor.gray.cgColor;
+        self.tabBar.layer.shadowOpacity = 0.5
+        self.tabBar.layer.shadowOffset = CGSize(width: 0, height: -1)
+        self.tabBar.layer.shadowRadius = 5        
+        
         self.setViewControllers([homeViewController, calenderViewController], animated: false);
     }
 }
