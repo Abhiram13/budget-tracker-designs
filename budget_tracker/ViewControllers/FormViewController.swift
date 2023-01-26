30831a193690ed49d031d633961ace0f3fba1231
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2023-01-26 11:33:10 +0530

    added popup view controller and passing data to parent

--- budget_tracker/ViewControllers/FormViewController.swift
+++ budget_tracker/ViewControllers/FormViewController.swift
@@ -5,53 +5,32 @@ class FormViewController: UIViewController {
     let text = TextField();
     let button = UIButton();
     var toggle: Bool = false;
-    let picker = UIPickerView()
-    let Num = ["1","2","3","4","5","6"];
     let label = UILabel();
-    let clearText = UITextField();
-    let toolbar = UIToolbar();
+    var txt: String = "";
     
     override func viewDidLoad() {
-        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed));
         super.viewDidLoad();
         view.backgroundColor = .green;
         self.tabBarController?.tabBar.isHidden = true;
-//        view.addSubview(text);
-//        view.addSubview(button);
-//        view.addSubview(picker)
+        view.addSubview(text);
+        view.addSubview(button);
         view.addSubview(label);
-        view.addSubview(clearText);
-        
-        picker.delegate = self;
-        picker.dataSource = self;
-        
-        toolbar.sizeToFit();
-        toolbar.setItems([doneBtn], animated: true);
         
         label.text = "Hello World";
         label.isUserInteractionEnabled = false;
         label.translatesAutoresizingMaskIntoConstraints = false;
         label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true;
         
-        clearText.translatesAutoresizingMaskIntoConstraints = false;
-        clearText.topAnchor.constraint(equalTo: label.topAnchor).isActive = true;
-        clearText.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true;
-        clearText.trailingAnchor.constraint(equalTo: label.trailingAnchor).isActive = true;
-        clearText.bottomAnchor.constraint(equalTo: label.bottomAnchor).isActive = true;
-        clearText.backgroundColor = .clear;
-        clearText.inputAccessoryView = toolbar;
-        clearText.inputView = picker;
-        
-//        text.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true;
-//        text.heightAnchor.constraint(equalToConstant: 50).isActive = true;
-//        text.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true;
-//        text.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true;
-//
-//        button.setTitle("Button", for: .normal)
-//        button.addTarget(self, action: #selector(click), for: .touchUpInside);
-//        button.translatesAutoresizingMaskIntoConstraints = false;
-//        button.topAnchor.constraint(equalTo: text.bottomAnchor, constant: 20).isActive = true;
-//        button.leadingAnchor.constraint(equalTo: text.leadingAnchor).isActive = true;
+        text.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true;
+        text.heightAnchor.constraint(equalToConstant: 50).isActive = true;
+        text.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true;
+        text.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true;
+
+        button.setTitle("Button", for: .normal)
+        button.addTarget(self, action: #selector(click), for: .touchUpInside);
+        button.translatesAutoresizingMaskIntoConstraints = false;
+        button.topAnchor.constraint(equalTo: text.bottomAnchor, constant: 20).isActive = true;
+        button.leadingAnchor.constraint(equalTo: text.leadingAnchor).isActive = true;
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -59,30 +38,12 @@ class FormViewController: UIViewController {
     }
     
     @objc private func click() {
+        print("First: \(txt)");
         self.modalPresentationStyle = .fullScreen;
-//        self.present(ModalView(), animated: true, completion: nil);
-    }
-    
-    @objc private func donePressed() {
-        self.view.endEditing(true);
-    }
-}
-
-extension FormViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return Num.count;
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Num[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        label.text = Num[row];
+        self.present(ChildViewController() { data in
+            self.txt = data;
+        }, animated: true, completion: nil);
+        print("Second: \(txt)");
     }
 }
 
@@ -96,4 +57,53 @@ func TextField() -> UITextField {
     textField.layer.cornerRadius = 10;
     
     return textField;
+}
+
+class ChildViewController: UIViewController {
+    var callBack: ((_ data: String) -> Void);
+    let banks: [String] = ["Text one", "Text one", "Text one", "Text one"];
+    let stack = UIStackView();
+    let scroll = UIScrollView();
+    
+    required init?(coder: NSCoder) {
+        self.callBack = {(data: String) -> Void in return};
+        super.init(coder: coder);
+    }
+    
+    init(callback: @escaping (String) -> Void) {
+        self.callBack = callback;
+        super.init(nibName: nil, bundle: nil);
+    }
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white;
+        self.callBack("asdh");
+        
+        view.addSubview(scroll);
+        scroll.addSubview(stack);
+        
+        scroll.translatesAutoresizingMaskIntoConstraints = false;
+        scroll.contentSize = CGSize(width: view.frame.width, height: 1000);
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false;
+        stack.backgroundColor = .yellow;
+        stack.axis = .vertical;
+        stack.distribution = .equalSpacing;
+        
+        for bank in banks {
+            let label = UILabel();
+            label.text = bank;
+            label.translatesAutoresizingMaskIntoConstraints = false;
+            label.heightAnchor.constraint(equalToConstant: 50).isActive = true;
+            stack.addArrangedSubview(label);
+        }
+        
+        NSLayoutConstraint.activate([
+            scroll.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scroll.heightAnchor.constraint(equalTo: view.heightAnchor),
+            stack.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30),
+            stack.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ]);
+    }
 }
